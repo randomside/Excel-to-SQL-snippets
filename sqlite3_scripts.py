a3cbfe316ad209d28097cd26b963b7f0bc823acc
Thinh-NT <thinhnt.helloworld@gmail.com>
2021-06-28 06:43:40 +0700

    Add exception

--- sqlite3_scripts.py
+++ sqlite3_scripts.py
@@ -4,18 +4,14 @@ import pandas as pd
 dbname = 'students_performance'
 conn = sqlite3.connect(dbname + '.sqlite')
 
-# SQL COMMANDS
-# cur = conn.cursor()
-# cur.execute('SELECT * FROM Table1')
-# for row in cur:
-#     print(row)
-
 chunksize = 10000
 for chunk in pd.read_csv('students_performance.csv', chunksize=chunksize):
     # replacing spaces with underscores for column names
     chunk.columns = chunk.columns.str.replace(' ', '_')
-    chunk.to_sql(name='Performance', con=conn,)
-
+    try:
+        chunk.to_sql(name='Performance', con=conn)
+    except ValueError:
+        chunk.to_sql(name='Performance', con=conn, if_exists='append')
 
 cur = conn.cursor()
 cur.execute('SELECT * FROM Performance')
